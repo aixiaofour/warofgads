46be20d072a19f4bb08b5d3b291415f45285ae5b
aixiaofour <1325869794@qq.com>
2023-03-02 14:18:23 +0800

    fyrox version init

---
+++ game/src/lib.rs
@@ -0,0 +1,88 @@
+//! Game project.
+use fyrox::{
+    core::pool::Handle,
+    event::Event,
+    event_loop::ControlFlow,
+    gui::message::UiMessage,
+    plugin::{Plugin, PluginConstructor, PluginContext, PluginRegistrationContext},
+    scene::{Scene, loader::AsyncSceneLoader},
+    utils::log::Log
+};
+
+pub struct GameConstructor;
+
+impl PluginConstructor for GameConstructor {
+    fn register(&self, _context: PluginRegistrationContext) {
+        // Register your scripts here.
+    }
+
+    fn create_instance(
+        &self,
+        override_scene: Handle<Scene>,
+        context: PluginContext,
+    ) -> Box<dyn Plugin> {
+        Box::new(Game::new(override_scene, context))
+    }
+}
+
+pub struct Game {
+    scene: Handle<Scene>,
+    loader: Option<AsyncSceneLoader>,
+}
+
+impl Game {
+    pub fn new(override_scene: Handle<Scene>, context: PluginContext) -> Self {
+        let mut loader = None;
+        let scene = if override_scene.is_some() {
+            override_scene
+        } else {
+            loader = Some(AsyncSceneLoader::begin_loading(
+                "data/scene.rgs".into(),
+                context.serialization_context.clone(),
+                context.resource_manager.clone(),
+            ));
+            Default::default()
+        };
+
+        Self { scene, loader }
+    }
+}
+
+impl Plugin for Game {
+    fn on_deinit(&mut self, _context: PluginContext) {
+        // Do a cleanup here.
+    }
+
+    fn update(&mut self, context: &mut PluginContext, _control_flow: &mut ControlFlow) {
+         if let Some(loader) = self.loader.as_ref() {
+            if let Some(result) = loader.fetch_result() {
+                match result {
+                    Ok(scene) => {
+                        self.scene = context.scenes.add(scene);
+                    }
+                    Err(err) => Log::err(err),
+                }
+            }
+        }
+    
+        // Add your global update code here.
+    }
+
+    fn on_os_event(
+        &mut self,
+        _event: &Event<()>,
+        _context: PluginContext,
+        _control_flow: &mut ControlFlow,
+    ) {
+        // Do something on OS event here.
+    }
+
+    fn on_ui_message(
+        &mut self,
+        _context: &mut PluginContext,
+        _message: &UiMessage,
+        _control_flow: &mut ControlFlow,
+    ) {
+        // Handle UI events here.
+    }
+}
